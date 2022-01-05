cf25081633a56594e746a585613b817193541d68
mbrav <mbrav@protonmail.com>
2022-01-05 08:22:23 +0300

    make modules

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,51 @@
 // Host: developer.mozilla.org
 // Accept-Language: fr
 
-fn main() {
-    let get = Method::GET;
-    let delete = Method::DELETE;
-    let post = Method::POST;
-    let put = Method::PUT;
+use http::request::Request;
+use server::Server;
 
+fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr: addr }
+mod server {
+    pub struct Server {
+        addr: String,
     }
 
-    fn run(self) {
-        println!("Server running on {}", self.addr)
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self { addr: addr }
+        }
+
+        pub fn run(self) {
+            println!("Server running on {}", self.addr)
+        }
     }
 }
 
-struct Request {
-    path: String,
-    query: Option<String>,
-    method: String,
-}
+mod http {
+    pub mod request {
+        use super::method::Method;
+        pub struct Request {
+            path: String,
+            query: Option<String>,
+            method: Method,
+        }
+    }
 
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
+    pub mod method {
+        pub enum Method {
+            GET,
+            DELETE,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
+    }
 }
